a6086e19622075bb2dca23e0ea55de085094a29c
Gwenael8389 <gwenael.drouet@alumni.enac.fr>
2026-05-18 09:38:24 +0200

    caca

---
+++ app/portail_auth/__init__.py
@@ -0,0 +1,4 @@
+"""
+Portail d'authentification : enregistre le blueprint pour login, register, password recovery.
+Gère la création et validation des sessions utilisateur.
+"""
---
+++ tests/test_admin.py
@@ -0,0 +1,4 @@
+"""
+Tests du portail admin : CRUD vols/avions, contrôle d'accès, intégrité des données.
+Vérifie les restrictions d'accès et les validations métier.
+"""
