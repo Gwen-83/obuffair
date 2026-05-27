0af48cf49e7c0614915f266264419f7723c225e4
Gwenael8389 <gwenael.drouet@alumni.enac.fr>
2026-05-27 15:50:08 +0200

    del

--- tests/test_client.py
+++
@@ -1,4 +0,0 @@
-"""
-Tests du portail client : recherche vols, création réservation, modifications compte.
-Vérifie le parcours complet client et les cas d'erreur (vol plein, données invalides).
-"""
