6dbce0166e7ca1c00290578924f0b1d7bac63c1b
Antoine Hardy <antoine.hardy.149@gmail.com>
2026-06-10 10:11:43 +0200

    Supprimer fichier non utilisés

--- app/algos/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Exporte les algorithmes (tarification, yield management, gestion escales).
-Facilite l'accès centralisé aux logiques calculs.
-"""
--- app/algos/escale.py
+++
@@ -1,4 +0,0 @@
-"""
-Gestion des escales : calcule les durées minimales, temps de correspondance.
-Propose les meilleurs itinéraires avec escales optimales.
-"""
